162ade5b9af79e158f8bd0b9512f8f244bb3e84e
League Student <league@iMac.attlocal.net>
2019-05-19 17:05:38 -0700

    adsg

--- src/_02_Chat_Application/ChatApp2.java
+++ src/_02_Chat_Application/ChatApp2.java
@@ -1,5 +1,8 @@
 package _02_Chat_Application;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -11,54 +14,59 @@ import javax.swing.JTextField;
 
 import _00_Click_Chat.networking.Server;
 
-public class ChatApp2 {
-	
+public class ChatApp2 implements KeyListener {
+
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-	//JLabel label = new JLabel();
-	
+	// JLabel label = new JLabel();
+
 	JTextArea textArea = new JTextArea(30, 100);
-	
+
 	JTextField textField = new JTextField(25);
 	JButton button = new JButton();
-	
+
 	JScrollPane scroll = new JScrollPane(textArea);
-	
+
 	String messages = "";
 
 	ChatServer2 server;
 	ChatClient2 client;
 	
+	boolean serversend = true;
+
 	public static void main(String[] args) {
 		new ChatApp2();
 	}
-	
+
 	public ChatApp2() {
-		
+
 		textArea.setEditable(false);
 		frame.add(panel);
-		panel.add(/*label*/ scroll);
+		panel.add(/* label */ scroll);
 		panel.add(textField);
 		button.setText("SEND");
 		panel.add(button);
+		textField.addKeyListener(this);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		int response = JOptionPane.showConfirmDialog(null, "Would you like to host a connection?", "Buttons!", JOptionPane.YES_NO_OPTION);
-		
-		if(response == JOptionPane.YES_OPTION) {
-			
+
+		int response = JOptionPane.showConfirmDialog(null, "Would you like to host a connection?", "Buttons!",
+				JOptionPane.YES_NO_OPTION);
+
+		if (response == JOptionPane.YES_OPTION) {
+
 			String name = JOptionPane.showInputDialog("Who are you?");
-			server = new ChatServer2(8080, /*label*/ textArea, name);
+			server = new ChatServer2(8080, textArea, name);
 			frame.setTitle(name);
 			JOptionPane.showMessageDialog(null, "ip: " + server.getIPAddress() + ", port: " + server.getPort());
+			serversend = true;
 			button.addActionListener((e) -> {
 				String thing = textField.getText();
 				textField.setText("");
 				server.sendText(thing);
 			});
 			server.start();
-			
+
 		} else {
 
 			String ipStr = JOptionPane.showInputDialog("Enter the IP Address");
@@ -66,15 +74,41 @@ public class ChatApp2 {
 			String name = JOptionPane.showInputDialog("Who are you?");
 			frame.setTitle(name);
 			int port = Integer.parseInt(prtStr);
-			client = new ChatClient2(ipStr, port, /*label*/ textArea, name);
+			serversend = false;
+			client = new ChatClient2(ipStr, port, textArea, name);
 			button.addActionListener((e) -> {
 				String thing = textField.getText();
 				textField.setText("");
 				client.sendText(thing);
 			});
 			client.start();
-			
+
 		}
-		
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyChar() == (KeyEvent.VK_ENTER)) {
+			String thing = textField.getText();
+			textField.setText("");
+			if (serversend) {
+				server.sendText(thing);
+			} else {
+				client.sendText(thing);
+			}
+		}
 	}
 }
